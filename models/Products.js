7086a41dc101902ad51fe0476de63f5343d27d4b
Astrid Timana <astrid.1295@gmail.com>
2021-08-08 19:48:16 -0500

    modeling

---
+++ models/Products.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const productSchema = new Schema({
+  
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,     
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    dateEntry: {
+      type: Date,
+      default: Date.now,
+    },
+  
+});
+
+module.exports = model('Products', productSchema);
