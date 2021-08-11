81b6d676af38b12aedf48d06d73075413b9f5d00
astridtimana <astrid.1295@gmail.com>
2021-08-11 10:49:25 -0500

    userschema bcrypt

--- models/Users.js
+++ models/Users.js
@@ -20,4 +20,21 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.pre('save', (next)=>{
+
+  let user = this
+  if(!user.isModified('password')) return next()
+  bcrypt.genSalt(10,(err,salt)=>{
+    if(err) return next()
+    bcrypt.hash(user.password,salt, null,(err,hash)=>{
+      if(err) return next(err)
+      user.password=hash
+      next()
+    })
+  })
+
+})
+
+
+
 module.exports = model('Users', userSchema);
