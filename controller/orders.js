663e354d560df0df9691da04eb17909bc71d4a9a
carlasanchezcorrea <carlasanchezcorrea@gmail.com>
2021-08-24 04:52:08 -0500

    test put bad props OK

--- controller/orders.js
+++ controller/orders.js
@@ -3,14 +3,13 @@ const Order = require('../models/Orders')
 const getOrder = async (req, res) => {
   try {
     let orderId = req.params.orderId
-    if(Object.isVALID(orderId)){
+    console.log(orderId)
       await Order.findById(orderId, (err, order)=>{
-        if (err) return res.status(500).send({message:`Error en la petición orderID`})
+        if (err) return res.status(404).send({message:`Error en la petición orderID`})
         if (!order) return res.status(404).send({message:`Order no existe`})
-    
-        res.status(200).send({ order })
+       // console.log(order.products[1].product.name); // sí muestra en el post
+        res.status(200).send( order)
       })
-    }
    
   } catch (error) {
     return res.status(404).send('Error')
@@ -58,10 +57,12 @@ const saveOrder= async (req, res,next) => {
 
 }
 
-const updateOrder = async (req, res) => {
+const updateOrder = async (req, res, next) => {
   try {
     let orderId = req.params.orderId
     let update = req.body
+
+    if(Object.keys(update).length == 0){ return next(400)}
 
     const orderUpdate= await Order.findByIdAndUpdate(
       orderId,
