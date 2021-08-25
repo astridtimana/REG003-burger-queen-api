40e553741a2d2c98b87d176f4a89d2b2d9bf57a9
carlasanchezcorrea <carlasanchezcorrea@gmail.com>
2021-08-25 08:51:46 -0500

    auth router

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
-const jwt = require('jsonwebtoken');
-const config = require('../config');
-const User = require('../models/Users')
-const { secret } = config;
-const bcrypt = require('bcrypt')
+const {
+  signIn
+} = require('../controller/auth');
+
+
 /** @module auth */
 module.exports = (app, nextMain) => {
   /**
@@ -17,47 +17,7 @@ module.exports = (app, nextMain) => {
    * @code {400} si no se proveen `email` o `password` o ninguno de los dos
    * @auth No requiere autenticación
    */
-   app.post('/auth', async (req, res, next) => {
-
-  try{
-
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return next(400);
-    }
-
-    Object.keys(req.body).length == 0 && next(400)
-    Object.keys(req.body.email).length == 0 && next(400)
-
-    const user = await User.findOne({ email: req.body.email });
-      // console.log(doc)
-      bcrypt.compare( req.body.password, user.password,
-        (err,data)=> {
-        if(err){console.info(err)} 
-        
-        else if(data) {return next(console.log(':C'), 404)}
- 
-        const token = jwt.sign({ id: user._id, roles:user.roles, email:user.email}, config.secret, {
-          expiresIn: 60 * 60 *6,
-        });
-
-        res.status(200).send({ auth: true, token });
-    
-        // TODO: autenticar a la usuarix 2° - Ready
-       //next();
-       }  
-      );
-    if (!user) {
-      return res.status(404).send("The email doesn't exists");
-    }
-
-    res.status(200)
-
-  } catch (error) {
-    return next(404);
-  }
-
-  });
+   app.post('/auth', signIn);
 
   return nextMain();
 };
