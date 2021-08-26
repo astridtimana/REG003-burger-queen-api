d19db15ba6acfa9c6f63a9b52aecd567dbc761f1
carlasanchezcorrea <carlasanchezcorrea@gmail.com>
2021-08-26 11:55:12 -0500

    funciones user.js-controller

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,5 @@
+const objectId = require("mongoose").Types.ObjectId; // es un schemaType de objectId de mongoose
+
 const linkHeader = (limit, page, totalPages , url, response) =>{
  const link = {
     first: `${url}?limit=${limit}&page=1`,
@@ -6,8 +8,19 @@ const linkHeader = (limit, page, totalPages , url, response) =>{
     last: `${url}?limit=${limit}&page=${totalPages}`,
 }
     return link;
-}
+};
+
+const validObjectId= (id) => objectId.isValid(id);
+
+const validEmail = (email) => {
+    let regEx = /\S+@\S+/;
+    return regEx.test(email);
+};
+
+
 
 module.exports={
-    linkHeader
+    linkHeader,
+    validObjectId,
+    validEmail
 }
